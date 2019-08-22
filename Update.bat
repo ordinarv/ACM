93a76572ee9fe549a6a35dfd773853e6a3ee021b
ordinarv <1132636886@qq.com>
2019-08-22 16:09:20 +0800

    'Update'

--- Update.bat
+++ Update.bat
@@ -1,8 +1,3 @@
-echo 当前目录是：%D:\ACM%
-
-git add -A .
-echo;
-git commit -m "Update"
-echo;
-git push origin master
-pause
+"C:\Program Files\Git\bin\git.exe" add .
+"C:\Program Files\Git\bin\git.exe" commit -m 'Update'
+"C:\Program Files\Git\bin\git.exe" push
--- test.bat
+++
@@ -1,3 +0,0 @@
-"C:\Program Files\Git\bin\git.exe" add .
-"C:\Program Files\Git\bin\git.exe" commit -m 'Update'
-"C:\Program Files\Git\bin\git.exe" push
